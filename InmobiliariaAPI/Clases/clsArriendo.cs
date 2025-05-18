9317c7a9a260cd4407616fcb0c561eef3cc587de
Santiago <santiagoaristizabal329018@correo.itm.edu.co>
2025-05-18 11:57:46 -0500

    feat: servicios de arriendo y transacion (CRUD)

---
+++ InmobiliariaAPI/Clases/clsArriendo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using InmobiliariaAPI.Models;
+
+namespace InmobiliariaAPI.Clases
+{
+    public class clsArriendo
+    {
+
+        private DBINMOBILIARIAEntities DBinmovi = new DBINMOBILIARIAEntities();
+        public ARRIENDO arriendo { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                DBinmovi.ARRIENDOes.Add(arriendo);
+                DBinmovi.SaveChanges();
+                return "Arriendo registrado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al registrar el arriendo: " + ex.Message;
+            }
+        }
+
+        public ARRIENDO ConsultarXCodigo(int codigo)
+        {
+            ARRIENDO arri = DBinmovi.ARRIENDOes.FirstOrDefault(a => a.Codigo_Transaccion == codigo);
+            return arri;
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                ARRIENDO arri = ConsultarXCodigo(arriendo.Codigo_Transaccion);
+                if (arri == null) 
+                {
+                    return "Arriendo no encontrado";
+                }
+                DBinmovi.ARRIENDOes.AddOrUpdate(arriendo);
+                DBinmovi.SaveChanges();
+                return "Arriendo con codigo" + arriendo.Codigo_Transaccion + " actualizado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al actualizar el arriendo: " + ex.Message;
+            }
+        }
+
+        public string Eliminar(int codigo)
+        {
+            try
+            {
+                ARRIENDO arri = ConsultarXCodigo(codigo);
+                if (arri == null)
+                {
+                    return "Arriendo no encontrado";
+                }
+                DBinmovi.ARRIENDOes.Remove(arriendo);
+                DBinmovi.SaveChanges();
+                return "Arriendo con codigo " + codigo + " eliminado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al eliminar el arriendo: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ InmobiliariaAPI/Clases/clsTransacion.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using InmobiliariaAPI.Models;
+
+namespace InmobiliariaAPI.Clases
+{
+    public class clsTransacion
+    {
+        private DBINMOBILIARIAEntities DBinmovi = new DBINMOBILIARIAEntities();
+        public TRANSACCION transacion { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                DBinmovi.TRANSACCIONs.Add(transacion);
+                DBinmovi.SaveChanges();
+                return "Transacción registrada correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al registrar la transacción: " + ex.Message;
+            }
+        }
+
+        public TRANSACCION ConsultarXCodigo (int codigo)
+        {
+            TRANSACCION tran = DBinmovi.TRANSACCIONs.FirstOrDefault(t => t.Codigo_Transaccion == codigo);
+            return tran;
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                TRANSACCION tran = ConsultarXCodigo(transacion.Codigo_Transaccion);
+                if (tran == null)
+                {
+                    return "Transacción no encontrada";
+                }
+                DBinmovi.TRANSACCIONs.AddOrUpdate(transacion);
+                DBinmovi.SaveChanges();
+                return "Transacción con código " + transacion.Codigo_Transaccion + " actualizada correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al actualizar la transacción: " + ex.Message;
+            }
+        }
+        public string Eliminar (int codigo)
+        {
+            try
+            {
+                TRANSACCION tran = ConsultarXCodigo(codigo);
+                if (tran == null)
+                {
+                    return "Transacción no encontrada";
+                }
+                DBinmovi.TRANSACCIONs.Remove(tran);
+                DBinmovi.SaveChanges();
+                return "Transacción con código " + codigo + " eliminada correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al eliminar la transacción: " + ex.Message;
+            }
+        }
+    }
+}
