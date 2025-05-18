20ddd0dec18a2f1f85faf2fa2bb499a7ccf8634a
Yeison <187584546+SYeison@users.noreply.github.com>
2025-05-18 10:28:16 -0500

    feat: envio la clase y el controlador de empleado

---
+++ InmobiliariaAPI/Controllers/EmpleadoController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using InmobiliariaAPI.Models;
+using InmobiliariaAPI.Clases;
+
+namespace InmobiliariaAPI.Controllers
+{
+    public class EmpleadoController : ApiController
+    {
+        [RoutePrefix("api/Empleado")]
+        [Authorize]
+        public class clienteController : ApiController
+        {
+            [HttpPost]
+            [Route("Insertar")]
+            public string Insertar([FromBody] EMPLEADO dato)
+            {
+                clsEmpleado empleado = new clsEmpleado();
+                empleado.empleado = dato;
+                return empleado.Insertar();
+            }
+            [HttpGet]
+            [Route("ConsultarTodos")]
+            public List<EMPLEADO> ConsultarTodos()
+            {
+                //Se crea un objeto de la clase clsEmpleado
+                clsEmpleado empleado = new clsEmpleado();
+                //Se llama al método ConsultarTodos de la clase clsEmpleado
+                return empleado.ConsultarTodo();
+            }
+            [HttpGet]
+            [Route("Consultar")]
+            public EMPLEADO Consultar(int Documento)
+            {
+                clsEmpleado empleado = new clsEmpleado();
+                return empleado.Consultar(Documento);
+            }
+            [HttpPut]
+            [Route("Actualizar")]
+            public string Actualizar([FromBody] EMPLEADO empleado)
+            {
+                clsEmpleado emplead = new clsEmpleado();
+                emplead.empleado = empleado;
+                return emplead.Actualizar();
+            }
+            [HttpDelete]
+            [Route("Eliminar")]
+            public string Eliminar([FromBody] EMPLEADO empleado)
+            {
+                clsEmpleado emplead = new clsEmpleado();
+                emplead.empleado = empleado;
+                return emplead.Eliminar();
+            }
+        }
+    }
+}
