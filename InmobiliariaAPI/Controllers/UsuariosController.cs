ed504a11d0dba8b403d07f63f83383b8d255a33d
ArsJohan <133719384+ArsJohan@users.noreply.github.com>
2025-05-18 11:53:02 -0500

    feat: Servicios Cypher (Cifrado de contraseñas) feat: Servicios Login (Inicio de Sesion y Validación de BearToken JWT) feat: Servicios Usuario (Creación de Usuario) feat: Servicios TokenGenerator (Generar bearToken) y TokenValidatorHandler (Validar BearToken) feat: Dtos Login (Estructura de envio), LoginResponse (Estrucutra de Respuesta) feat: Controllers Login y Usuarios (Comunicación de API con los Servicios)

---
+++ InmobiliariaAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,25 @@
+﻿using InmobiliariaAPI.Clases;
+using InmobiliariaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace InmobiliariaAPI.Controllers
+{
+    [RoutePrefix("api/Usuarios")]
+    public class UsuariosController : ApiController
+    {
+        [HttpPost]
+        [Route("CrearUsuarios")]
+        [Authorize]
+        public string CrearUsuarios([FromBody] USUARIO usuario, int idPerfil)
+        {
+            clsUsuario _usuario = new clsUsuario();
+            _usuario.usuario = usuario;
+            return _usuario.CrearUsuario(idPerfil);
+        }
+    }
+}
--- InmobiliariaAPI/Dtos/DtoLogin.cs
+++ InmobiliariaAPI/Dtos/DtoLogin.cs
@@ -7,5 +7,18 @@ namespace InmobiliariaAPI.Dtos
 {
 	public class DtoLogin
 	{
-	}
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public string PaginaSolicitud { get; set; }
+    }
+
+    public class DtoLoginResponse
+    {
+        public string Usuario { get; set; }
+        public string Perfil { get; set; }
+        public string PaginaInicio { get; set; }
+        public bool Autenticado { get; set; }
+        public string Token { get; set; }
+        public string Mensaje { get; set; }
+    }
 }
